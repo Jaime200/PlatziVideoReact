55b45afd7dac263a669981577c89264df62bfc12
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-01 01:52:11 -0600

    Se utiliza distintas secciones de categorias utilizando la propiedad title

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -3,7 +3,7 @@ import Header from  '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
-import CarouselItem from '../components/carouselItem';
+import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
@@ -11,13 +11,25 @@ const App = () =>(
     <div className="App">
         <Header />
         <Search />
-        <Categories >
+        <Categories title="Mi lista" >
             <Carousel>
                 <CarouselItem/>
                 <CarouselItem/>
                 <CarouselItem/>
                 <CarouselItem/>
+            </Carousel>           
+        </Categories>
+        <Categories title="Tendencias" >
+        <Carousel >
                 <CarouselItem/>
+                <CarouselItem/>                
+            </Carousel>
+        </Categories>
+        <Categories  title="Originales de Platzi vídeo">
+            <Carousel>
+                <CarouselItem/>
+                <CarouselItem/>
+                <CarouselItem/>                
             </Carousel>
         </Categories>
         <Footer />
