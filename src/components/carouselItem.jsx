5a3ea9ec8a4ba7454d284493516672f597d22fb1
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-02 02:33:27 -0600

    Se agrega el manejo de propTypes en el archivo de carouselItem.jsx

--- src/components/carouselItem.jsx
+++ src/components/carouselItem.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import '../assets/styles/components/CarouselItem.scss'
-import playIcon from '../assets/static/play-icon.png'
-import plusIcon from '../assets/static/plus-icon.png'
+import React from 'react';
+import  PropTypes from 'prop-types';
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
+
 const CarouselItem = ({ cover, title, year, contentRating, duration }) =>(
     <div className="carousel-item">
     <img className="carousel-item__img" src={cover} alt={title}  />
@@ -15,5 +17,11 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) =>(
     </div>
   </div>
 )
-
+CarouselItem.propTypes = { 
+  cover: PropTypes.string, 
+  title: PropTypes.string, 
+  year: PropTypes.number, 
+  contentRating: PropTypes.string, 
+  duration : PropTypes.number
+};
 export default CarouselItem;
