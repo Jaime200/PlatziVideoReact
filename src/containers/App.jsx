eca5b6e7d360b6e88ee34962c1c6a8f5cd443db6
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-02 01:22:02 -0600

    Se hace el uso de hooks el uso de fetch para obtener datos

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,7 +8,7 @@ import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
 const App = () =>{
-    const [ videos, setVideos ] = useState([]);
+    const [ videos, setVideos ] = useState({mylist: [], trends: [], originals: [] });
     useEffect( ()=>{        
         fetch('http://localhost:3000/initalState')
         .then( response => response.json() )
@@ -21,18 +21,21 @@ const App = () =>{
     <div className="App">
             <Header />
             <Search />
-            <Categories title="Mi lista" >
-                <Carousel>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                    <CarouselItem/>
-                </Carousel>           
-            </Categories>
+            {videos.mylist.length > 0 && 
+                <Categories title="Mi lista" >
+                    <Carousel>
+                        <CarouselItem  key={item.id} {...item}/>
+                    </Carousel>           
+                </Categories>
+            }
+           
             <Categories title="Tendencias" >
             <Carousel >
-                    <CarouselItem/>
-                    <CarouselItem/>                
+                {videos.trends.map(item =>{
+                    console.log(item);
+                    return <CarouselItem key={item.id} {...item }/>                    
+                    })
+                }                                
                 </Carousel>
             </Categories>
             <Categories  title="Originales de Platzi vídeo">
