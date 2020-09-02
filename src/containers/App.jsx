ed33be73cce7245f6c9613a240c4ae2795744331
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-02 00:07:09 -0600

    Se utiliza hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useState, useEffect  } from 'react';
 import Header from  '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,33 +7,44 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
-const App = () =>(
+const App = () =>{
+    const [ videos, setVideos ] = useState([]);
+    useEffect( ()=>{        
+        fetch('http://localhost:3000/initalState')
+        .then( response => response.json() )
+        .then( data => setVideos(data));
+
+    },[]);
+
+    console.log(videos);
+    return (
     <div className="App">
-        <Header />
-        <Search />
-        <Categories title="Mi lista" >
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>           
-        </Categories>
-        <Categories title="Tendencias" >
-        <Carousel >
-                <CarouselItem/>
-                <CarouselItem/>                
-            </Carousel>
-        </Categories>
-        <Categories  title="Originales de Platzi vídeo">
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>                
-            </Carousel>
-        </Categories>
-        <Footer />
-    </div>
-);
+            <Header />
+            <Search />
+            <Categories title="Mi lista" >
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                </Carousel>           
+            </Categories>
+            <Categories title="Tendencias" >
+            <Carousel >
+                    <CarouselItem/>
+                    <CarouselItem/>                
+                </Carousel>
+            </Categories>
+            <Categories  title="Originales de Platzi vídeo">
+                <Carousel>
+                    <CarouselItem/>
+                    <CarouselItem/>
+                    <CarouselItem/>                
+                </Carousel>
+            </Categories>
+            <Footer />
+        </div>
+        );
+}
 
 export default App;
