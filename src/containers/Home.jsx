c6cc082552b6f4133271e6366fcaa1c7d010e297
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-15 01:01:55 -0600

    Se modifica myListt (error)  a myList(correcto), no renderizaba

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -15,7 +15,7 @@ const Home = ({ myList, trends, originals }) =>{
             {myList !==undefined &&  myList.length > 0 &&
             <Categories  title="Mi lista">
                 <Carousel>
-                    { myListt.map(item =>{
+                    { myList.map(item =>{
                         return <CarouselItem key={item.id} {...item }/>           
                         })
                     }                
