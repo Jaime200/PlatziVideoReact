50ea90152dfa42805b81b17a924fc8983f33008d
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-15 01:18:23 -0600

    Se agrega nuevo boton de remove y action de delete

--- src/components/carouselItem.jsx
+++ src/components/carouselItem.jsx
@@ -4,7 +4,8 @@ import  PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
-import { setFavorite } from '../actions';
+import removeIcon from '../assets/static/remove-icon.png'
+import { setFavorite, deleteFavorite } from '../actions';
 
 const CarouselItem = (props) =>{
   const { id, cover, title, year, contentRating, duration } = props;
@@ -12,6 +13,10 @@ const CarouselItem = (props) =>{
     props.setFavorite({
       id, cover, title, year, contentRating, duration
     })
+  }
+
+  const handleDeleteFavorite = (itemId)=>{
+    props.deleteFavorite(itemId)
   }
   
   return (
@@ -26,6 +31,10 @@ const CarouselItem = (props) =>{
             src={plusIcon} 
             alt="Plus Icon"
             onClick={handleSetFavorite}/> 
+         <img className="carousel-item__details--img" 
+            src={removeIcon} 
+            alt="Remove Icon"
+            onClick={()=>handleDeleteFavorite(id)}/> 
       </div>
       <p className="carousel-item__details--title">{title}</p>
     <p className="carousel-item__details--subtitle">{` ${ year } ${ contentRating } ${duration} `}</p>
@@ -43,6 +52,7 @@ CarouselItem.propTypes = {
   duration : PropTypes.number
 };
 const mapDispatchToProps = {
-  setFavorite
+  setFavorite,
+  deleteFavorite
 }
 export default connect(null,mapDispatchToProps)(CarouselItem)
