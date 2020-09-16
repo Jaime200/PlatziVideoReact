bf7b361740c7248a9e71497c85276d22feb1189e
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-09-16 17:48:01 -0600

    Se realiza el action y reducer de logout así como la paramitizacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,14 @@ import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 import { Link } from 'react-router-dom'
+import { logoutRequest } from '../actions'
 const Header = (props) =>{
   const { user } = props
   const hasUser = Object.keys(user).length >0;
+
+  const handleLogout = ()=>{
+    props.logoutRequest({})
+  }
   return (
       <header className="header">
         <Link to="/">          
@@ -24,12 +29,22 @@ const Header = (props) =>{
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href="/">Cuenta</a></li>
-          <li>
-            <Link to="/login">
-            Iniciar sesión
-            </Link>          
+          
+          {hasUser ?
+          <li><a href="/">{user.name}</a></li>
+            :      null
+          }
+          {hasUser ? 
+            <li>
+              <a href="#logout" onClick={handleLogout}>Cerrar Sesión</a>         
             </li>
+            :
+            <li> 
+              <Link to="/login"> Iniciar sesión</Link>            
+            </li>
+          }
+            
+          
         </ul>
       </div>
     </header>
@@ -40,4 +55,8 @@ const mapStateToProp = state =>{
     user : state.user
   }
 }
-export default connect(mapStateToProp,null)(Header)
+
+const mapDispacthToProps = {
+  logoutRequest
+}
+export default connect(mapStateToProp,mapDispacthToProps)(Header)
